079c9351e8654ee3a1c889e1e59d28dff024d9b4
Tobiasfib <72310273+tobijae@users.noreply.github.com>
2025-01-13 15:36:18 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RepoPreview from './components/RepoPreview';
+
+function App() {
+  return (
+    <div className="App">
+      <RepoPreview />
+    </div>
+  );
+}
+
+export default App;
